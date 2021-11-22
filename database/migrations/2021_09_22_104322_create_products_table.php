283b03016a2228e4bab4abd6ebcd531bf1913930
JoristerWolbeek <joris.terwolbeek@outlook.com>
2021-11-22 10:46:54 +0100

    added images to migration for practice

--- database/migrations/2021_09_22_104322_create_products_table.php
+++ database/migrations/2021_09_22_104322_create_products_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -19,8 +20,9 @@ class CreateProductsTable extends Migration
                 $table->id();
                 $table->timestamps();
                 $table->string('name');
-                $table->boolean('on_list')->default("false");
-                $table->bigInteger('user_id')->unsigned();
+                $table->string('image')->nullable();
+                $table->boolean('on_list')->default(false);
+                $table->foreignIdFor(User::class);
             }
         );
 
