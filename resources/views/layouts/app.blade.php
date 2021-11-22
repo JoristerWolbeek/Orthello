6935ce7607a8dcb03fb4c7343b03b7922cc56800
JoristerWolbeek <joris.terwolbeek@outlook.com>
2021-11-22 10:45:59 +0100

    changed directories and file names, page wont jump to top

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,64 +27,69 @@
 <body>
     <div id="app" class="">
         <nav class=" navbar navbar-expand-md navbar-light shadow-sm bg-secondary">
-        <div class="container">
-            <a class="navbar-brand text-primary" href="{{ url('/') }}">
-                {{ config('app.name', 'Laravel') }}
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+            <div class="container">
+                <a class="navbar-brand text-primary" href="{{ url('/') }}">
+                    {{ config('app.name', 'Laravel') }}
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                @guest
-                    <!-- Left Side Of Navbar -->
-                @else
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item">
-                            <a class="nav-link text-primary" href="{{ route('list') }}">{{ __('Shopping list') }}</a>
-                        </li>
-                    </ul>
-                @endguest
-
-                <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav ml-auto">
-
-                    <!-- Authentication Links -->
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     @guest
-                        @if (Route::has('login'))
-                            <li class="nav-item">
-                                <a class="nav-link text-primary" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                        @endif
-
-                        @if (Route::has('register'))
-                            <li class="nav-item">
-                                <a class="nav-link text-primary" href="{{ route('register') }}">{{ __('Register') }}</a>
-                            </li>
-                        @endif
+                        <!-- Left Side Of Navbar -->
                     @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle text-primary" href="#" role="button" data-toggle="dropdown" aria-haspopup="true"
-                                aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }}
-                            </a>
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item">
+                                <a class="nav-link text-primary" href="{{ url('/list') }}">{{ __('Shopping list') }}</a>
+                            </li>
+                        </ul>
+                    @endguest
 
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                             document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+
+                        <!-- Authentication Links -->
+                        @guest
+                            @if (Route::has('login'))
+                                <li class="nav-item">
+                                    <a class="nav-link text-primary" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                </li>
+                            @endif
+
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link text-primary" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle text-primary" href="#" role="button" data-toggle="dropdown"
+                                    aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
                                 </a>
 
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
-                    @endguest
-                </ul>
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                                                 document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                    <a class="dropdown-item" href="{{ url('/products') }}">
+                                        {{ __('Products') }}
+                                    </a>
+
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
             </div>
-        </div>
         </nav>
 
         <main class="py-4">
---
+++ resources/views/product/index.blade.php
@@ -0,0 +1,71 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        @if ($errors->any())
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                @foreach ($errors->all() as $error)
+                    <p>{{ $error }}</p>
+                @endforeach
+            </div>
+        @endif
+        <form method="post" action="../list/add" autocomplete="off">
+            <input hidden value="{{ Auth::user()->id }}" name="id">
+            <div class="input-group mb-3">
+                <input type="text" id="search" name="name" class="form-control" placeholder="Chocolate" aria-describedby="basic-addon2">
+                <div class="input-group-append">
+                    <button class="btn btn-primary" type="submit">Add to shopping list</button>
+                </div>
+            </div>
+            @csrf
+        </form>
+        <div class="list-group">
+            @foreach ($products->sortBy(request('sort')) as $product)
+                <div class="d-flex mt-3 justify-content-between">
+                    <div class="list-group-item list-group-item-action d-flex justify-content-between bg-secundary">
+                        <a class="text-secondary" href="#/"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="100%" fill="currentColor"
+                                class="bi bi-caret-down" viewBox="0 0 16 16" data-toggle="collapse" data-target="#collapseExample{{ $product->id }}"
+                                aria-expanded="false" aria-controls="collapseExample{{ $product->id }}">
+                                <path
+                                    d="M3.204 5h9.592L8 10.481 3.204 5zm-.753.659 4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659z" />
+                            </svg>
+                        </a>
+                        <h3 style="margin: 0px">{{ $product->name }}</h3>
+                        <div>
+                            <form method="POST" action="{{ url('list/delete') }}">
+                                {{ method_field('PUT') }}
+                                <input value="Delete" type="submit" class="btn btn-danger">
+                                <input hidden value="{{ $product->id }}" name="id">
+                                @csrf
+                            </form>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="collapse " id="collapseExample{{ $product->id }}">
+                    <div class="container list-group px-5 my-5">
+                        <form method="POST" action="{{ url('products/update') }}">
+                            @csrf
+                            {{ method_field('PUT') }}
+                            <div class="form-floating mb-3">
+                                <label for="product">Product</label>
+                                <input required class="form-control" id="name" type="text" placeholder="product" data-sb-validations="required" />
+                            </div>
+                            <div class="form-floating mb-3">
+                                <label for="newField2">Image</label>
+                                <input class="form-control" id="img" type="file" placeholder="New Field 2" />
+                            </div>
+                            <div class="d-grid">
+                                <input class="btn btn-primary btn-lg" id="submitButton" value="Change" type="submit"></button>
+                            </div>
+                        </form>
+                    </div>
+
+                </div>
+            @endforeach
+            <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+
+        @endsection
